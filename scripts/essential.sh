8cb45d30745b3d99aa5202fcee7b8d868a2c90a9
DujinA <dujinaleksandar@gmail.com>
2025-07-23 15:15:33 +0200

    added .gitconfig

--- scripts/essential.sh
+++ scripts/essential.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 sudo pacman -Syu
-sudo pacman -S git curl ripgrep bat pavucontrol xclip jq yq tldr shutter python-pip nvidia nvidia-utils nvidia-settings rofi stylua btop gimp
+sudo pacman -S git curl git-delta ripgrep bat pavucontrol xclip jq yq tldr shutter python-pip nvidia nvidia-utils nvidia-settings rofi stylua btop gimp
 
 git clone git@github.com:junegunn/fzf.git $HOME/personal/fzf
 $HOME/personal/fzf/install
