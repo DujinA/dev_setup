5464dafa67a7f8c35187b959e336afd7b4708adf
DujinA <dujinaleksandar@gmail.com>
2025-09-21 14:41:40 +0200

    update

--- scripts/essential.sh
+++ scripts/essential.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 sudo pacman -Syu
-sudo pacman -S git curl git-delta ripgrep bat pavucontrol xclip jq yq tldr shutter python-pip nvidia nvidia-utils nvidia-settings wofi stylua btop gimp bluez blueman
+sudo pacman -S git-delta pavucontrol xclip yq python-pip nvidia-settings lua51 stylua gimp bluez blueman
 
 git clone git@github.com:junegunn/fzf.git $HOME/personal/fzf
 $HOME/personal/fzf/install
